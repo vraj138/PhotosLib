8dee31dffb45b0353e50e99d84326a2a8cffda32
Deep Parekh <dparekh2019@gmail.com>
2022-11-23 15:08:41 -0500

    commented out json parts

--- src/controller/SceneA1Controller.java
+++ src/controller/SceneA1Controller.java
@@ -15,43 +15,42 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import org.json.JSONArray;
-
 import application.User;
 
 public class SceneA1Controller {
-    @FXML ListView<User> listView;
+    @FXML
+    ListView<User> listView;
 
     private ObservableList<User> obListUsers;
 
-    public void start(Stage mainStage) throws IOException{
+    public void start(Stage mainStage) throws IOException {
         String content = readFile("userdata/usrerLists.json");
 
-        JSONArray userArray = new JSONArray(content);
         ArrayList<User> users = new ArrayList<User>();
 
-        for(int i = 0; i < userArray.length(); i++) {
-            User fromstorage = new User(userArray.getJSONObject(i).getString("username"));
-            users.add(fromstorage);
-        }
+        // for (int i = 0; i < userArray.length(); i++) {
+        // User fromstorage = new
+        // User(userArray.getJSONObject(i).getString("username"));
+        // users.add(fromstorage);
+        // }
 
         obListUsers = FXCollections.observableArrayList(users);
-		listView.setItems(obListUsers);
+        listView.setItems(obListUsers);
     }
 
     private String readFile(String path) throws IOException {
-		File file = new File(path);
-		
-		// Build a string from file's contents
-		StringBuilder content = new StringBuilder((int)file.length());
-		
-		try (Scanner scanner = new Scanner(file)) {
-			while(scanner.hasNextLine()) {
-				content.append(scanner.nextLine() + System.lineSeparator());
-			}
-			return content.toString();
-		} 
-	}
+        File file = new File(path);
+
+        // Build a string from file's contents
+        StringBuilder content = new StringBuilder((int) file.length());
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                content.append(scanner.nextLine() + System.lineSeparator());
+            }
+            return content.toString();
+        }
+    }
 
     @FXML
     void onAddUser(ActionEvent event) throws IOException {
@@ -63,17 +62,17 @@ public class SceneA1Controller {
 
         TextField input = userDialog.getEditor();
 
-        if(result.isPresent() && input.getText() != null && input.getText().toString().length() != 0){
+        if (result.isPresent() && input.getText() != null && input.getText().toString().length() != 0) {
             User newUser = new User(input.getText().trim());
             System.out.println("Added " + newUser);
             obListUsers.add(newUser);
-            
+
             listView.getSelectionModel().clearSelection();
             listView.getSelectionModel().select(obListUsers.indexOf(newUser));
 
-            saveUsers(obListUsers);
+            // saveUsers(obListUsers);
             return;
-        }else{
+        } else {
             userDialog.close();
         }
     }
@@ -83,10 +82,10 @@ public class SceneA1Controller {
 
     }
 
-    public void saveUsers(ObservableList<User> userList) throws IOException {
-		JSONArray userLists = new JSONArray(obListUsers);
-		try (FileWriter file = new FileWriter("userdata/userLists.json")) {
-			file.write(userLists.toString());
-        }
-    }
+    // public void saveUsers(ObservableList<User> userList) throws IOException {
+    // JSONArray userLists = new JSONArray(obListUsers);
+    // try (FileWriter file = new FileWriter("userdata/userLists.json")) {
+    // file.write(userLists.toString());
+    // }
+    // }
 }
