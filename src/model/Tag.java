09436e6f410ed7f83010b8dd763bcc704273201a
Deep Parekh <dparekh2019@gmail.com>
2022-11-25 11:40:10 -0500

    added Tag model

--- src/model/Tag.java
+++ src/model/Tag.java
@@ -1,5 +1,20 @@
 package model;
 
-public class Tag {
-    
+import java.io.Serializable;
+
+public class Tag implements Serializable {
+    public String tagName;
+    public String value;
+
+    // we need a method to check if 2 tags are equal (both by name and by value)
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || obj instanceof Tag) {
+            return false;
+        }
+
+        Tag t = (Tag) obj;
+
+        return t.tagName.equals(this.tagName) && t.value.equals(this.value);
+    }
 }
